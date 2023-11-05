6ebb4d8156bfd3d9fccd8aa0ea8b5c1b55ed06e0
Educpf <eduardocportugalf@gmail.com>
2023-11-05 03:25:26 +0000

    Finished documentation of Class and implemented removeStudent method

--- Class.cpp
+++ Class.cpp
@@ -5,59 +5,30 @@
 
 using namespace std;
 
-/**
- * @brief Standard constructor of the class Class.
- * @details Time complexity: O(1)
- */
+
 
 Class::Class() {}
 
-/**
- * @brief Constructor of the class Class. ucCode and classCode are set to the
- * given values.
- * @details Time complexity: O(1)
- * @param ucCode code of the UC
- * @param classCode code of the class
- */
 
 Class::Class(const string& ucCode, const string& classCode)
     : ucCode(ucCode), classCode(classCode) {}
 
-/**
- * @brief Returns the ucCode of the class.
- * @details Time complexity: O(1)
- * @return ucCode code of the UC of the class
- */
 
 string Class::getUcCode() const { return this->ucCode; }
 
-/**
- * @brief Returns the classCode of the class.
- * @details Time complexity: O(1)
- * @return classCode code of the class
- */
 
 string Class::getClassCode() const { return this->classCode; }
 
-/**
- * @brief Adds a new class schedule to the classSchedules
- * @details Time complexity: O(1)
- * @param newClassSchedule new schedule of a class of the class
- */
 
 void Class::addClassSchedule(const ClassSchedule& newClassSchedule) {
     this->classSchedules.push_back(newClassSchedule);
 }
 
-/**
- * @brief Returns all the class's class schedules.
- * @details Time complexity: O(1)
- * @return classSchedules all the classes schedules of the class
- */
 
 vector<ClassSchedule> Class::getAllClassSchedules() const {
     return this->classSchedules;
 }
+
 
 bool Class::invalidOverlaps(const Class& destinyClass) const {
     for (const auto& originalSchedule : classSchedules) {
@@ -70,46 +41,23 @@ bool Class::invalidOverlaps(const Class& destinyClass) const {
     return false;
 }
 
-/**
- * @brief Adds a new student code to the students in the class
- * @details Time complexity: O(log n)
- * @param newStudentCode student code of a new student enrolled in the class
- */
 
 void Class::addStudent(const int newStudentCode) {
     this->students.insert(newStudentCode);
 }
 
-/**
- * @brief Removes a student code from the students in the class
- * @details Time complexity: TODO
- * @param studentCode student code of the student to be removed
- */
 
 void Class::removeStudent(const int studentCode) {
-    // TODO
+    this->students.erase(studentCode);
 }
 
-/**
- * @brief Returns all the class students.
- * @details Time complexity: O(1)
- * @return students all the codes of the students enrolled in the class
- */
 
 set<int> Class::getAllStudents() const { return this->students; }
 
-/*
- *@brief Returns the numbers of students in the class
- *@details Time complexity: O(1)
- *@return Number of students in the class
- */
+
 int Class::numberStudents() const { return this->students.size(); }
 
-/*
- *@brief Checks if the class has vacancy
- *@details Time Complexity: O(1)
- *@return True if the class has vacancy, otherwise False
- */
+
 bool Class::hasVacancies() const { return (numberStudents() < MAX_STUDENTS); }
 
 
--- Class.h
+++ Class.h
@@ -21,28 +21,147 @@ class Class {
     const static int MAX_STUDENTS = 30;
 
    public:
-    // Class constructor
+    // CLASS CONSTRUCTORS
+    /**
+ * @brief Default constructor for the Class.
+ *
+ * @details This default constructor creates a new instance of the Class with no specific
+ * UC code or class code. It can be used to initialize an empty Class.
+ * @details Time complexity: O(1)
+     */
     Class();
+    /**
+ * @brief Class constructor with UC code and class code.
+ *
+ * @details Initializes a new instance of the Class with the provided UC code and class code.
+ * @details Time complexity: O(1)
+ *
+ * @param ucCode A string representing the UC (Unit Course) code associated with the Class.
+ * @param classCode A string representing the unique code of the Class.
+     */
     Class(const string& ucCode, const string& classCode);
 
-    // Set & Getter -> ucCode
+    // GETTERS
+    /**
+ * @brief Getter method to retrieve the UC code of the Class.
+ *
+ * @details This method returns the UC code associated with the Class.
+ * @details Time complexity: O(1)
+ *
+ * @return A string containing the UC code of the Class.
+     */
     string getUcCode() const;
-
-    // Set & Getter -> classCode
+    /**
+ * @brief Getter method to retrieve the Class code of the Class.
+ *
+ *
+ *
+ * @details This method returns the Class code associated with the Class.
+ * @details Time complexity: O(1)
+ *
+ * @return A string containing the Class code of the Class.
+     */
     string getClassCode() const;
 
-    // classSchedules related methods
+    // ClassSchedule related methods
+    /**
+ * @brief Adds a new class schedule to the Class.
+ *
+ * @details This method appends a new class schedule, represented by 'newClassSchedule',
+ * to the list of schedules associated with the Class.
+ * @details Time complexity: O(1)
+ *
+ * @param newClassSchedule The ClassSchedule to add to the Class.
+     */
     void addClassSchedule(const ClassSchedule& newClassSchedule);
+    /**
+ * @brief Retrieves all class schedules associated with the Class.
+ *
+ * @details This method returns a vector containing all the class schedules associated
+ * with the Class, providing access to the complete list of schedules.
+ * @details Time complexity: O(1)
+ *
+ * @return A vector of ClassSchedule objects representing the class schedules.
+     */
     vector<ClassSchedule> getAllClassSchedules() const;
+    /**
+ * @brief Checks for invalid overlaps between two classes' schedules.
+ *
+ * @details This method iterates through the schedules of the current class and the
+ * provided 'destinyClass', comparing each pair of schedules to check if they
+ * have invalid overlaps. An overlap is considered invalid if it conflicts with
+ * the timing of another schedule.
+ * @details Time complexity: Sem complexity
+ *
+ * @param destinyClass The Class to compare schedules with.
+ *
+ * @return true if there are invalid overlaps between the schedules of the
+ * current class and the 'destinyClass'; otherwise, false.
+     */
     bool invalidOverlaps(const Class& other) const;
 
-    // students related methods
+    // Students related methods
+    /**
+ * @brief Adds a new student (by student code) to the Class.
+ *
+ * @details This method adds a new student to the Class by including the student's code
+ * in the list of students associated with the Class.
+ * @details Time complexity: O(log n)
+ *
+ * @param newStudentCode An integer representing the code of the new student.
+     */
     void addStudent(int newStudentCode);
+    /**
+ * @brief Removes a student from the Class by student code.
+ *
+ * @details This method removes a student from the Class based on the provided student code.
+ * If a student with the given code is found in the Class, they will be removed.
+ * @details Time complexity: O(log n)
+ *
+ * @param studentCode An integer representing the code of the student to be removed.
+     */
     void removeStudent(int studentCode);
+    /**
+ * @brief Retrieves all students' codes associated with the Class.
+ *
+ * @details This method returns a set containing the student codes of all students
+ * associated with the Class, allowing access to the complete list of students.
+ * @details Time complexity: O(1)
+ *
+ * @return A set of integers representing the student codes.
+     */
     set<int> getAllStudents() const;
+    /**
+ * @brief Retrieves the number of students enrolled in the Class.
+ *
+ * @details This method returns the count of students currently associated with the Class,
+ * providing the total number of students.
+ * @details Time complexity: O(1)
+ *
+ * @return An integer representing the number of students.
+     */
     int numberStudents() const;
-
+    // Vacancy related methods
+    /**
+ * @brief Checks if the Class has available vacancies for students.
+ *
+ * @details This method checks whether the Class has available vacancies for students by
+ * comparing the number of enrolled students with the maximum allowed capacity.
+ * @details Time complexity: O(1)
+ *
+ * @return true if there are vacancies in the Class; false if the maximum
+ * capacity has been reached.
+     */
     bool hasVacancies() const;
+    /**
+ * @brief Retrieves the number of available vacancies in the Class.
+ *
+ * @details This method calculates and returns the number of available vacancies in the Class
+ * by subtracting the current number of enrolled students from the maximum allowed capacity.
+ * @details Time complexity: O(1)
+ *
+ * @return An integer representing the number of available vacancies in the Class.
+     */
     int numberVacancies() const;
 };
 

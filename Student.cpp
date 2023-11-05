6b981c47fb8743cd49842f3cfa8597de9094d97f
Educpf <eduardocportugalf@gmail.com>
2023-11-05 11:55:29 +0000

    Made documentation for students and changed minor details

--- Student.cpp
+++ Student.cpp
@@ -3,73 +3,35 @@
 #include "Student.h"
 using namespace std;
 
-/**
- * @brief Standard constructor of the class Student.
- * @details Time complexity: O(1)
- */
+
 
 Student::Student() = default;
 
-/**
- * @brief Constructor of the class Student. studentCode and studentName are set
- * to the given values.
- * @details Time complexity: O(1)
- * @param studentCode code of the student
- * @param studentName name of the student
- */
 
 Student::Student(int studentCode, const string& studentName) {
     this->studentCode = studentCode;
     this->studentName = studentName;
 }
 
-/**
- * @brief Returns the studentCode of the student.
- * @details Time complexity: O(1)
- * @return studentCode code of the student
- */
 
 int Student::getStudentCode() const { return this->studentCode; }
 
-/**
- * @brief Returns the studentName of the student.
- * @details Time complexity: O(1)
- * @return studentName name of the student
- */
 
 string Student::getStudentName() const { return this->studentName; }
 
-/**
- * @brief Adds a new class to the classes
- * @details Time complexity: O(log(n))
- * @param newClass new class of the student
- */
+
+map<string, Class> Student::getAllClasses() const { return this->classes; }
+
 
 void Student::addClass(const Class& newClass) {
     this->classes[newClass.getUcCode()] = newClass;
 }
 
-// VAI SER PARA MUDAR ?????? -> É MELHOR FAZER A TROCA ENTRE DUAS CLASSES NUM
-// MESMA UC (+ rapido)
-
-/**
- * @brief Removes a class from the classes in student
- * @details Time complexity: TODO
- * @param classCode class code of the class to be removed
- */
 
 void Student::removeClass(const string& ucCode) {
-    this->classes.at(ucCode).removeStudent(studentCode);
     this->classes.erase(ucCode);
 }
 
-/**
- * @brief Returns all the classes of student.
- * @details Time complexity: O(1)
- * @return classes all the classes of the student
- */
-
-map<string, Class> Student::getAllClasses() const { return this->classes; }
 
 string Student::findConflictClass(const std::string& ucCode, const std::string& originClassCode, const Class& destinyClass) const {
     for (const auto& [classUcCode, c] : classes) {
@@ -80,32 +42,24 @@ string Student::findConflictClass(const std::string& ucCode, const std::string& 
     return "";
 }
 
+
 string Student::getUcClassCode(const string& ucCode) const {
     return classes.at(ucCode).getClassCode();
 }
 
-/*
- *@brief Return the number of Ucs the student is in.
- *@details Time complexity: O(1)
- *@return the number of Ucs a student is in.
- */
 
 int Student::numberOfUcs() const { return classes.size(); }
 
-/*
- *@brief Checks if a students is enrolled in a UC
- *@details Time complexity: O(n)
- *@param ucCode Code of the UC to check
- *@return True if the student enrolles in that UC, otherwise False
- */
+
 bool Student::hasUc(const string& ucCode) const {
-    for (const auto [classCode, c] : classes) {
+    for (const auto& [classCode, c] : classes) {
         if (c.getUcCode() == ucCode) {
             return true;
         }
     }
     return false;
 }
+
 
 string Student::getStudentAsString() const {
     stringstream res;
--- Student.h
+++ Student.h
@@ -19,28 +19,143 @@ class Student {
     map<string, Class> classes;
 
    public:
-    // Student constructor
+    // CONSTRUCTORS
+    /**
+ * @brief Default constructor for the Student class.
+ *
+ * @details This default constructor creates a new instance of the Student class without
+ * specifying the student code or name. It initializes the Student with default values.
+ * @details Time complexity: O(1)
+     */
     Student();
+    /**
+ * @brief Constructor for the Student class.
+ *
+ * @details Initializes a new instance of the Student class with the provided student code
+ * and student name.
+ * @details Time complexity: O(1)
+ *
+ * @param studentCode An integer representing the unique code of the student.
+ * @param studentName A string representing the name of the student.
+     */
     Student(int studentCode, const string &studentName);
 
-    // Set & Getter -> studentCode
+    // GETTERS
+    /**
+ * @brief Retrieves the student code of the Student.
+ *
+ * @details This method returns the unique student code associated with the Student.
+ * @details Time complexity: O(1)
+ *
+ * @return An integer representing the student code.
+     */
     int getStudentCode() const;
-
-    // Set & Getter -> studentName
+    /**
+ * @brief Retrieves the name of the Student.
+ *
+ * @details This method returns the name of the Student.
+ * @details Time complexity: O(1)
+ *
+ * @return A string representing the name of the Student.
+     */
     string getStudentName() const;
-
-
-    // classes related methods
-    void addClass(const Class &newClass);
-    void removeClass(const string &ucCode);
+    /**
+ * @brief Retrieves all classes associated with the Student.
+ *
+ * @details Method returns a map containing all the classes associated with the Student.
+ * The map is keyed by the UC (Unit Course) code and contains Class objects.
+ * @details Time complexity: O(1)
+ *
+ * @return A map where the keys are UC codes, and the values are Class objects.
+     */
     map<string, Class> getAllClasses() const;
+
+    // Class related methods
+    /**
+    * @brief Adds a new Class to the Student's list of classes.
+    *
+    * @details This method adds a new Class, represented by 'newClass', to the Student's list
+    * of classes. The Class is associated with the UC (Unit Course) code, which is used as
+    * the key in the list of classes.
+    * @details Time complexity: O(log n)
+    *
+    * @param newClass The Class to add to the Student's list of classes.
+    */
+    void addClass(const Class &newClass);
+    /**
+ * @brief Removes a Class from the Student's list of classes by UC code.
+ *
+ * @details method removes a Class from the Student's list of classes based on the provided UC (Unit Course) code.
+ * @details Time complexity: O(log n)
+ *
+ * @param ucCode A string representing the UC code of the Class to be removed.
+     */
+    void removeClass(const string &ucCode);
+    /**
+ * @brief Finds a conflicting Class based on UC code and destination Class.
+ *
+ * @details Method searches for a conflicting Class among the Student's classes based
+ * on the provided UC code and destination Class. It checks if any of the Student's
+ * classes in the same UC overlap with the destination Class. If a conflict is found,
+ * it returns the Class code of the conflicting Class.
+ * @details Time complexity: sem complexity
+ *
+ * @param ucCode A string representing the UC (Unit Course) code of the destination Class.
+ * @param originClassCode A string representing the code of the Class the Student wants to leave.
+ * @param destinyClass The Class the Student wants to join.
+ *
+ * @return A string representing the Class code of the conflicting Class, or an empty string
+ * if no conflict is found.
+     */
     string findConflictClass(const string& ucCode, const string &originClassCode,
                      const Class& destinyClass) const;
+    /**
+ * @brief Retrieves the Class code of the Student for a specific UC.
+ *
+ * @details This method returns the Class code of the Student for the provided UC (Unit Course) code.
+ * @details Time complexity: O(log n)
+ *
+ * @param ucCode A string representing the UC code for which the Student's Class code is requested.
+ *
+ * @return A string representing the Class code of the Student in the specified UC, or an empty string
+ * if the UC code is not found in the Student's classes.
+     */
     string getUcClassCode(const string &ucCode) const;
-    // Ucs
+
+    // Ucs related methods
+    /**
+ * @brief Retrieves the number of Unit Courses (UCs) associated with the Student.
+ *
+ * @details This method returns the count of Unit Courses associated with the Student, which
+ * represents the number of UCs the Student is enrolled in.
+ * @details Time complexity: O(1)
+ *
+ * @return An integer representing the number of Unit Courses the Student is enrolled in.
+     */
     int numberOfUcs() const;
+    /**
+ * @brief Checks if the Student is enrolled in a specific Unit Course (UC).
+ *
+ * @details This method checks if the Student is enrolled in a Unit Course with the provided UC (Unit Course) code.
+ * It searches through the Student's classes to find a match.
+ * @details Time complexity: O(n)
+ *
+ * @param ucCode A string representing the UC code to check for enrollment.
+ *
+ * @return true if the Student is enrolled in the specified UC; otherwise, false.
+     */
     bool hasUc(const string &ucCode) const;
 
+    /**
+ * @brief Generates a formatted string representation of the Student's information.
+ *
+ * @details This method constructs a formatted string that represents the Student's information,
+ * including the student code, student name, and a list of the Unit Courses (UCs) and their
+ * associated Class codes that the Student is enrolled in.
+ * @details Time complexity: O(n)
+ *
+ * @return A string containing the formatted representation of the Student's information.
+     */
     string getStudentAsString() const;
 };
 
